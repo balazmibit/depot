f6ea4d575fb2acbeaed1c5288307992f5bb67d19
balazmibit <balaz@mibit.sk>
2014-08-07 00:04:16 +0200

    Nakupny kosik - kapitola 9 komplet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+  protect_from_forgery
+  
+  private
+  
+    # metoda current_cart zacina tak ze s objektu session berie hodnotu ulozenu pod klucom :cart_id ,
+    # pred tym ako sa pokusi vyhladat kosik odpovedajuci tomuto identifikacnemu cislu. 
+    # Ak nie je prislusny kosik najdeny , potom tato metoda pokracuje vytvorenim noveho objektu typu Cart a jeho 
+    # identifikacne cislo ulozi v session a potom novy kosik vrati.
+    def current_cart
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,11 @@
 class StoreController < ApplicationController
   def index
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
     # poziadame model Product o vypisanie vsetkym poloziek
     @products = Product.all
   end
+  
 end
